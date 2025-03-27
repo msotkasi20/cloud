443db0afc0f76e8ec58fc6f6be09b69b0e2d834e
msotkasi20 <msotkasi20@gmail.com>
2025-03-27 13:53:36 +0200

    Ensimmäinen testi

---
+++ bundleG/server.js
@@ -0,0 +1,20 @@
+// Luodaan node.js web-kehys express
+const express = require ('express');
+
+// Luodaan express-sovellus, jossa express() luo uuden sovelluksen
+// ja app on palvelin
+const app = express();
+
+// Määritellään reitti (route)
+app.get('/', (req, res) => {
+    res.send('Hello World from Render!');
+});
+
+// Määritetään käytettävä portti, 3000 on localhost portti
+const PORT = process.env.PORT || 3000
+
+// Käynnistetään palvelin ja tulostetaan viesti terminaaliin testiksi
+app.listen(PORT, () => {
+    console.log(`Server is running on port ${PORT}`);
+});
+
