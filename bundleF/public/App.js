90820b049ae7d6fe6fe0d0394e293c8f80e258b2
msotkasi20 <msotkasi20@gmail.com>
2025-04-23 11:55:30 +0300

    BundleF4

--- bundleF/public/App.js
+++ bundleF/public/App.js
@@ -1,6 +1,6 @@
 document.getElementById("hae").addEventListener("click", async () => {
     try {
-        const response = await fetch("https://<TÄHÄN-YOUR-RENDER-URL>.onrender.com/tapahtumat");
+        const response = await fetch("https://https://historia-backend-7tvi.onrender.com.onrender.com/tapahtumat");
         const data = await response.json();
         document.getElementById("tulokset").innerHTML = data.tapahtumat;
     } catch (error) {
